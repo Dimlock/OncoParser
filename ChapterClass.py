1316beb1bebb039351448e7e0ad28bcc5402d86e
Dimlock <roshin.artur@yandex.ru>
2024-12-18 12:10:08 +0300

    Добавил в класс главы класс параграфа с его статами и сериализировал его. Не обязательно удалять файлы. Всё равно же перезаписываю. А так это в гит новые добавлять надо

--- ChapterClass.py
+++ ChapterClass.py
@@ -1,12 +1,34 @@
 import ChapterParser
 import json
 
+
 class Chapter:
     def __init__(self, text_path):
         self.original_text = ChapterParser.parseText(text_path)
-        self.stats = ChapterParser.ProcessChapter(self.original_text)
+        self.paragraphs = self.create_paragraphs()
+
+    def create_paragraphs(self):
+        result = []
+        for paragraph in ChapterParser.splitOnParagraphs(self.original_text):
+            result.append(Paragraph(paragraph))
+        return result
 
     def save(self, save_name):
         with open(f"{save_name}.json", "w", encoding="UTF-8") as f:
             json.dump({"original_text": self.original_text,
-                    "stats": self.stats}, f, indent=4, ensure_ascii=False)
+                       "paragraphs": [paragraph.save() for paragraph in self.paragraphs]}, f, indent=4, ensure_ascii=False)
+
+
+class Paragraph:
+    def __init__(self, paragraph_text):
+        self.text = paragraph_text
+        self.stats = self.processParagraph()
+
+    def processParagraph(self):
+        processed_paragraph = ChapterParser.processParagraph(self.text)
+        result = ChapterParser.countTermFrequency(processed_paragraph)
+        return result
+
+    def save(self):
+        return {"text": self.text,
+                "stats": self.stats}
--- ChapterParser.py
+++ ChapterParser.py
@@ -6,24 +6,19 @@ def parseText(chapter_path):
         text = f.read()
     return text
 
-def _splitOnParagraphs(chapter_text):
+def splitOnParagraphs(chapter_text):
     return chapter_text.split("\n\n")
 
-def _processParagraph(paragraph):
+def processParagraph(paragraph):
     paragraph = paragraph.lower()
     paragraph = re.sub(r"\d+", "", paragraph)
     paragraph = re.sub(r"\W+", " ", paragraph)
     return paragraph
 
-def _countTermFrequency(paragraph):
+def countTermFrequency(paragraph):
     tokenized_paragraph = paragraph.split()
     words_count = len(tokenized_paragraph)
     counted_terms = Counter(tokenized_paragraph)
     term_frequency = {k: v/words_count for k,v in counted_terms.items()}
-    return Counter(term_frequency).most_common()
+    return Counter(term_frequency)
 
-def ProcessChapter(parsed_text):
-    paragraphs_list = _splitOnParagraphs(parsed_text)
-    processed_paragraphs = [_processParagraph(paragraph) for paragraph in paragraphs_list]
-    TF_paragraphs = [_countTermFrequency(paragraph) for paragraph in processed_paragraphs]
-    return TF_paragraphs
